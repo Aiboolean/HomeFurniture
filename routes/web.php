e85a20a34097e0800adcd6283929729e34ccb29a
Aiboolean <ivosorio77@gmail.com>
2024-10-11 22:47:15 +0800

    product can now be viewed on user side

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ use App\Http\Controllers\AdminController;
 
 Route::get('/', [HomeController::class,'home']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [HomeController::class,'login_home'])->middleware(['auth', 'verified'])->name('dashboard');
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
